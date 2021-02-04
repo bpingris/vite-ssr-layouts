db4f8882b638ac7a5c7cd8d7e2d3dab93efc7256
Benoit Pingris <benoit.pingris@pm.me>
2021-02-04 10:40:57 +0100

    feat: add tailwind

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import App from "./App.vue";
 import { createSSRApp } from "vue";
 import { createRouter } from "./router";
+import "./index.css";
 
 export function createApp() {
   const app = createSSRApp(App);
